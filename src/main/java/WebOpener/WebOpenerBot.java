77e6da25d3659f409edb20eab45bce7c7c4feb3a
JavaConterry <conterry05@gmail.com>
2022-04-29 19:12:27 +0300

    WIP

--- src/main/java/WebOpener/WebOpenerBot.java
+++ src/main/java/WebOpener/WebOpenerBot.java
@@ -2,31 +2,39 @@ package WebOpener;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebOpenerBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            String toSearch = update.getMessage().getText();
-            SendMessage message = new SendMessage();
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(toSearch);
-
             try {
+                String toSearch = update.getMessage().getText();
+                URL url = new URL("https://en.wikipedia.org/wiki/" + toSearch);
+                SendMessage message = new SendMessage();
+                message.setChatId(update.getMessage().getChatId().toString());
+                message.setText(toSearch);
                 execute(message);
                 Runtime.getRuntime()
                         .exec(new String[]{
-                                "cmd", "/c","start opera https://en.wikipedia.org/wiki/"+toSearch
+                                "cmd", "/c", "start opera " + url
                         });
-            } catch (IOException | TelegramApiException e) {
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+
+
+
         }
     }
 
